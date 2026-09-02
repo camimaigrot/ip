796a5f5b47f8a568d86a2750332474ce15c26dd7
Camille Maigrot <camille.maigrot.0542@gmail.com>
2026-09-02 16:26:45 +0800

    Refactor Ace class structure

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ace {
-    public static final String BANNER = "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+    public static final String BANNER = "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
         "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⣠⣄⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
         "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⣠⡾⣿⡟⠁⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿\n" +
         "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠁⠀⠿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿\n" +
@@ -15,30 +15,30 @@ public class Ace {
         "\t⣿⣿⣿⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
         "\t⣿⣿⣿⣿⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⣸⣿⡴⠟⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
         "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⠀⠻⠟⠁⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
-        "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n";
-    public static final String HL = "\t______________________________\n";
-    public static final String WELCOME_MESSAGE = "\tWelcome, Ace  here.\n";
-    public static final String ASSISTANCE_MESSAGE = "\tWhat can I do for you?\n";
-    public static final String BYE_MESSAGE = "\tCatch you later.\n";
+        "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿";
+    public static final String HL = "______________________________";
+    public static final String WELCOME_MESSAGE = "Welcome, Ace  here.";
+    public static final String ASSISTANCE_MESSAGE = "What can I do for you?";
+    public static final String BYE_MESSAGE = "Catch you later.";
 
     public static void printAceMessage(String message){
         printAceSeparation();
-        System.out.print("\t"+message);
+        System.out.print("\t"+message+"\n");
         printAceSeparation();
     }
 
     public static void printAceSeparation(){
-        System.out.print("\t"+HL);
+        System.out.print("\t"+HL+"\n");
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        printAceMessage(BANNER+HL+WELCOME_MESSAGE);
+        printAceMessage(BANNER);
+        printAceMessage(WELCOME_MESSAGE);
         printAceMessage(ASSISTANCE_MESSAGE);
         String line = in.nextLine();
         while(!line.contains("bye")){
             printAceMessage(line);
-            printAceSeparation();
             line = in.nextLine();
         }
         printAceMessage(BYE_MESSAGE);
